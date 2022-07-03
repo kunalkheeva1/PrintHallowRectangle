4b8e4e22746aedf6eb1e39cd8be5feae6cdd69ab
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-02 22:08:23 -0700

    I forgot to add the System.out.println() after the nested loop, instead i was adding it inside the loop.

--- src/PrintHallowRectangle.java
+++ src/PrintHallowRectangle.java
@@ -1,13 +1,19 @@
 public class PrintHallowRectangle {
     public static void printHallowRectangle(int row, int column){
 
-        for(int i=0; i<row; i++ ){
-            for(int j=0; j<column; j++){
-                if(i==1 || j==1 ||i==row || j==column ){
-                    System.out.print("*");
+        //get a nested loop
+        for(int i=0; i<=row; i++ ){
+            // one for the columns
+            for(int j=0; j<=column; j++){
+                //print star on the borders
+                if(i==0|| j==0  || i==row || j==column ){
+                    System.out.print(" *");
+                    //else space in between
+                }else{
+                    System.out.print("  ");
                 }
-                System.out.println();
-            }
+                //then move the cursor to the next line and go to i+1th index
+            }System.out.println();
         }
 
 
@@ -16,6 +22,6 @@ public class PrintHallowRectangle {
 
 
     public static void main(String[] args) {
-            printHallowRectangle(4,4);
+            printHallowRectangle(4,5);
     }
 }
