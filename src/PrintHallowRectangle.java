711e3eab88000cfdc757c2bdd2a41f227f387579
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-02 21:54:35 -0700

    created a method to print a hallow rectangle

--- src/PrintHallowRectangle.java
+++ src/PrintHallowRectangle.java
@@ -1,9 +1,21 @@
 public class PrintHallowRectangle {
-    
+    public static void printHallowRectangle(int row, int column){
+
+        for(int i=0; i<row; i++ ){
+            for(int j=0; j<column; j++){
+                if(i==1 || j==1 ||i==row || j==column ){
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+        }
+
+
+    }
 
 
 
     public static void main(String[] args) {
-
+            printHallowRectangle(4,4);
     }
 }
